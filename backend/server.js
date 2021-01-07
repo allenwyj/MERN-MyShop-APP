43c7cf4b3e96cc2268aa1cd1e96c531e962eb631
Yujie Wu <allenwuyujie@gmail.com>
2021-01-08 03:08:42 +1100

    created a simple backend server

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const products = require('./data/products');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+app.get('/api/products', (req, res) => {
+  // sending json data
+  res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find(product => product._id === req.params.id);
+  res.json(product);
+});
+
+app.get('/api/products', (req, res) => {
+  // sending json data
+  res.json(products);
+});
+
+app.listen(5000, console.log('Server running on port 5000'));
