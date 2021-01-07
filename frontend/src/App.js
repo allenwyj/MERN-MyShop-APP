4cd013e678fd4d8907c68763baeecca57bb189a1
Yujie Wu <allenwuyujie@gmail.com>
2021-01-08 02:01:40 +1100

    created Footer, Header and HomePage components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
+import HomePage from './pages/HomePage';
 
 const App = () => {
   return (
     <React.Fragment>
       <Header />
-      <h1>Welcome to MyShop</h1>
+      <main className="py-3">
+        <Container>
+          <HomePage />
+        </Container>
+      </main>
       <Footer />
     </React.Fragment>
   );
