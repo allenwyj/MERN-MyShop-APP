607b2b3b612b903425bd27110366aef602847385
Yujie Wu <allenwuyujie@gmail.com>
2021-01-07 23:38:09 +1100

    React setup

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Header />
+      <h1>Welcome to MyShop</h1>
+      <Footer />
+    </React.Fragment>
+  );
+};
+
+export default App;
