c7ec1d6e7b92e2a724fc31e3fc3df068e3ffff00
Yujie Wu <allenwuyujie@gmail.com>
2021-01-08 02:24:41 +1100

    imported react router and react router bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { Container } from 'react-bootstrap';
 import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
 
 const App = () => {
   return (
-    <React.Fragment>
-      <Header />
-      <main className="py-3">
-        <Container>
-          <HomePage />
-        </Container>
-      </main>
-      <Footer />
-    </React.Fragment>
+    <BrowserRouter>
+      <React.Fragment>
+        <Header />
+        <main className="py-3">
+          <Container>
+            <Route path="/" component={HomePage} exact />
+            <Route path="/product/:id" component={ProductPage} />
+          </Container>
+        </main>
+        <Footer />
+      </React.Fragment>
+    </BrowserRouter>
   );
 };
 
