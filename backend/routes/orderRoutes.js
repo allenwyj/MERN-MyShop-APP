0e10b31ebd6795a80157e6079f3ead2a23f864e8
Yujie Wu <allenwuyujie@gmail.com>
2021-01-11 15:15:53 +1100

    create orderRoutes

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,8 +1,11 @@
 import express from 'express';
 const router = express.Router();
-import { addOrderItems } from '../controllers/orderController.js';
+import { addOrderItems, getOrderById } from '../controllers/orderController.js';
 import { protect } from '../middlewares/authMiddleware.js';
 
 router.route('/').post(protect, addOrderItems);
+// should place after '/' route, otherwise, 
+// anything after '/' will be reconginised as id.
+router.route('/:id').get(protect, getOrderById);
 
 export default router;
