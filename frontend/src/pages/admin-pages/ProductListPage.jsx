d0b6695e7c8cfcffeed2d0a09e07cb989c5f3584
Yujie Wu <allenwuyujie@gmail.com>
2021-01-16 18:18:37 +1100

    hide create product button while loading

--- frontend/src/pages/admin-pages/ProductListPage.jsx
+++ frontend/src/pages/admin-pages/ProductListPage.jsx
@@ -78,9 +78,9 @@ const ProductList = ({ history, match }) => {
           <h1>Products</h1>
         </Col>
         <Col className="text-right">
-          <Button className="my-3" onClick={createProductHandler}>
+          {!loading && (<Button className="my-3" onClick={createProductHandler}>
             <i className="fas fa-plus"></i> Create Product
-          </Button>
+          </Button>)}
         </Col>
       </Row>
       {loadingDelete && <Loader />}
